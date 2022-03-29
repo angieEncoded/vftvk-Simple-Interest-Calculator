f629f17cee6734017a07435b8d3bfa3a5c4e6795
angieEncoded <43992078+angieEncoded@users.noreply.github.com>
2022-03-29 12:46:03 -0400

    Finish basic scaffolding

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+// Handle initial loading of page
+document.addEventListener('DOMContentLoaded', function () {
+    clearContents();
+    loadPage();
+})
+
+
+
+const loadPage = () => {
+    const contentRoot = document.getElementById("content-root");
+    const form = `
+
+    `
+    contentRoot.innerHTML = form;
+}
+
+const clearContents = () => {
+    const contentRoot = document.getElementById("content-root");
+    const resultsRoot = document.getElementById("results-root");
+    contentRoot.innerHTML = "";
+    resultsRoot.innerHTML = "";
+}
+
+
 function compute()
 {
     p = document.getElementById("principal").value;
-    
 }
         
